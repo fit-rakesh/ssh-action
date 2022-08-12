59563ea092f196e597ab25ae0dbf9ec64a49d6b8
Rakesh Tholiya <71389445+fit-rakesh@users.noreply.github.com>
2022-08-12 18:17:43 +0530

    Update ssh-ex.sh

--- ssh-ex.sh
+++ ssh-ex.sh
@@ -1,5 +1,3 @@
-eval $(ssh-agent -s)
-echo "$INPUT_KEY" | tr -d '\r' | ssh-add - > /dev/null
 echo "$INPUT_CMD"
-ssh -o StrictHostKeyChecking=no $INPUT_USERNAME@$INPUT_HOST $INPUT_CMD
+$INPUT_CMD
 echo "end"
